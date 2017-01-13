4e72f81a42ba02f166c9590d0f38cf3c40e7767e
brandonmchin <brandonmchin21@gmail.com>
2017-01-12 20:28:06 -0500

    add week 1 assignment

---
+++ Week1/globitek/private/validation_functions.php
@@ -0,0 +1,18 @@
+<?php
+
+  // is_blank('abcd')
+  function is_blank($value='') {
+    // TODO
+  }
+
+  // has_length('abcd', ['min' => 3, 'max' => 5])
+  function has_length($value, $options=array()) {
+    // TODO
+  }
+
+  // has_valid_email_format('test@test.com')
+  function has_valid_email_format($value) {
+    // TODO
+  }
+
+?>
---
+++ Week1/globitek/public/register.php
@@ -0,0 +1,52 @@
+<?php
+  require_once('../private/initialize.php');
+
+  // Set default values for all variables the page needs.
+
+  // if this is a POST request, process the form
+  // Hint: private/functions.php can help
+
+    // Confirm that POST values are present before accessing them.
+
+    // Perform Validations
+    // Hint: Write these in private/validation_functions.php
+
+    // if there were no errors, submit data to database
+
+      // Write SQL INSERT statement
+      // $sql = "";
+
+      // For INSERT statments, $result is just true/false
+      // $result = db_query($db, $sql);
+      // if($result) {
+      //   db_close($db);
+
+      //   TODO redirect user to success page
+
+      // } else {
+      //   // The SQL INSERT statement failed.
+      //   // Just show the error, not the form
+      //   echo db_error($db);
+      //   db_close($db);
+      //   exit;
+      // }
+
+?>
+
+<?php $page_title = 'Register'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <h1>Register</h1>
+  <p>Register to become a Globitek Partner.</p>
+
+  <?php
+    // TODO: display any form errors here
+    // Hint: private/functions.php can help
+  ?>
+
+  <!-- TODO: HTML form goes here -->
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
